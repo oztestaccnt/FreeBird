712ee7b72f48d098b0400f5ad6921c84494f98f1
oztestaccnt <oztestaccnt@gmail.com>
2018-10-03 01:31:10 -0500

    FreeBird Assgmnt

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OzLearningHub
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "FreeBird",
+                url: "Lessons/Lesson7_9/{name}/{age}/{color}",
+                defaults: new
+                {
+                    controller = "Lessons",
+                    action = "Lesson7_9",
+                    name = UrlParameter.Optional,
+                    age = UrlParameter.Optional,
+                    color = UrlParameter.Optional
+                },
+                constraints: new { }
+            );
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Models/FreeBirdModel.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OzLearningHub.Models
+{
+    public class FreeBirdModel
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Color { get; set; }
+    }
+}
